9228ebb476b81813ba0bdde5e5adeccfbc9d42ce
childebert-bouaichi <childebert.bouaichi@laplateforme.io>
2025-11-28 10:47:02 +0100

    ajout du fichier childebert-bouaichi.py

---
+++ childebert-bouaichi.py
@@ -0,0 +1,18 @@
+nom = input("Entrez votre nom : ")
+nb = input ("Entrez un chiffre entre 1-10 : ")
+def saluer(nom):
+    return "Bonjour " + nom + " !"
+
+
+
+while int(nb) > 10:
+    nb = input("Je vous ai dit de selectionner un nombre de 1 à 10 :")
+    if int(nb) <=10:
+        break
+
+print(saluer(nom))
+print("Voici votre nombre : ",nb)
+
+saisi = input("Veuillez saisir le nombre de fois que Python c'est cool s'affiche : ")
+for i in range(int(saisi)):
+    print("Python c'est cool ! ")
