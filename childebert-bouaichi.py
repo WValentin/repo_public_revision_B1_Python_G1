ea576495c4e1acbce2b3f438cd71c65201bef4b2
childebert-bouaichi <childebert.bouaichi@laplateforme.io>
2025-11-28 11:39:57 +0100

    revision childebert bouaichi

--- childebert-bouaichi.py
+++ childebert-bouaichi.py
@@ -5,7 +5,7 @@ def saluer(nom):
 
 
 
-while int(nb) > 10:
+while int(nb) > 10 or int(nb) < 1:
     nb = input("Je vous ai dit de selectionner un nombre de 1 à 10 :")
     if int(nb) <=10:
         break
@@ -14,5 +14,5 @@ print(saluer(nom))
 print("Voici votre nombre : ",nb)
 
 saisi = input("Veuillez saisir le nombre de fois que Python c'est cool s'affiche : ")
-for i in range(int(saisi)):
+for i in range(1,int(saisi)):
     print("Python c'est cool ! ")
